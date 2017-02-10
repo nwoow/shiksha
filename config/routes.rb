0293dad262a7e005186da08e4fb61508f6cd67ab
Nilay <nilayy1234@gmai.com>
2017-02-10 22:08:45 +0530

    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :homes
+
+
+namespace :admin do
+  constraints subdomain: 'admin' do
+    devise_for :users
+    resources :homes
+  end
+end
+  # constraints subdomain: 'admin' do
+  #   resources :homes
+  #   devise_for :users
+  # end
   root 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
